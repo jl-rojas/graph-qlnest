9fbb7e5c3089f32e29568b0f70292c3285b7e5d7
Jorge Rojas <jl.rojas@viaducto.io>
2021-03-05 16:21:06 -0600

    sory we

---
+++ src/items/items.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Args } from '@nestjs/graphql';
+import { InputItem, InputUpdateItem } from './inputs/items.input';
+import { ItemsService } from './items.service';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemService: ItemsService) {}
+
+  @Get()
+  async getItems() {
+    return await this.itemService.getItems();
+  }
+
+  @Get(':id')
+  async getItem(@Args(':id') id: String) {
+    return await this.itemService.getItem(id);
+  }
+
+  @Delete(':id')
+  async deleteItem(@Args('id') id: String) {
+    return await this.itemService.deleteItem(id);
+  }
+
+  @Post()
+  async createNewItem(@Args('data') data: InputItem) {
+    return await this.itemService.createItem(data);
+  }
+
+  @Put(':id')
+  async updateItem(
+    @Args('id') id: string,
+    @Args('item') item: InputUpdateItem,
+  ) {
+    return await this.itemService.updateItem(id, item);
+  }
+}
--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsService } from './items.service';
 import { ItemEntity } from './items.entity';
 import { ItemsResolver } from './items.resolver';
+import { ItemsController } from './items.controller';
 
 @Module({
   providers: [ItemsService, ItemsResolver],
-  imports: [ TypeOrmModule.forFeature([ ItemEntity ])],
-  exports: [ItemsService]
+  imports: [TypeOrmModule.forFeature([ItemEntity])],
+  exports: [ItemsService],
+  controllers: [ItemsController],
 })
 export class ItemsModule {}
