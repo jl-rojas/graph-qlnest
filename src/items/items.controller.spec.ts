37e7e149791488dc2726459c437ce3c525e2d34c
Juan Olvera <jp.oes@hotmail.com>
2021-03-09 19:23:51 -0600

    add tests

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { ItemsController } from './items.controller';
+import { ItemEntity } from './items.entity';
+import { ItemsService } from './items.service';
 
 describe('ItemsController', () => {
-  let controller: ItemsController;
-
+  let itemsController: ItemsController;
+  let itemsService: ItemsService;
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [ItemsController],
+      providers: [
+        ItemsService,
+        {
+          provide: getRepositoryToken(ItemEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<ItemsController>(ItemsController);
+    itemsService = moduleRef.get<ItemsService>(ItemsService);
+    itemsController = moduleRef.get<ItemsController>(ItemsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  test('should be defined', () => {
+    expect(itemsService).toBeDefined();
+  });
+
+  it('should return an array of items', async () => {
+    const result = [
+      {
+        id: '1',
+        name: 'mantecadas',
+        price: 15,
+        stock: 15,
+        price_preferred: 10,
+      },
+    ];
+
+    jest.spyOn(itemsService, 'getItems').mockResolvedValue(result);
+
+    expect(await itemsController.getItems()).toBe(result);
   });
 });
